6ea2d6a3c72c140aea4b43e9ff6f6b494f21f06c
mari hiramoto <maritoronto23@gmail.com>
2024-03-07 16:35:57 -0500

    move sound, getSound(), setSound(), and  makeSound() function definition to dog.h. source_cpp demonstrates polymorphism

--- mari_dog.h
+++ mari_dog.h
@@ -2,9 +2,28 @@
 #include "animal.h"
 
 class Dog : public Animal {
+private:
+	string sound;
 public:
-	Dog(string n = "New Dog", string s = "Bark!", double w = 0.0, int h = 0);
+	Dog(string n = "New Dog", string l = "Location", string s = "Bark!", double w = 0.0, int h = 0);
+	void setSound(string s);
+	string getSound() const;
+	virtual void makeSound() const override;
+	virtual void displayReport() const override;
 };
 
-Dog::Dog(string n, string s, double w, int h)
-	: Animal(n, s, w, h) {}
+Dog::Dog(string n, string l, string s, double w, int h)
+	: Animal(n, l, w, h) {
+	setSound(s);
+}
+
+void Dog::setSound(string s) { sound = s; }
+string Dog::getSound() const { return sound; }
+
+void Dog::makeSound() const {
+	cout << "\n" << getName() << " says: " << getSound() << endl;
+}
+void Dog::displayReport() const {
+	Animal::displayReport();
+	cout << "Sound: " << getSound() << endl;
+}
--- mari_source.cpp
+++ mari_source.cpp
@@ -2,22 +2,11 @@
 #include "dog.h"
 
 int main() {
-	Animal newAnimal;
-	newAnimal.displayReport();
+	Dog newDog("Scratch", "Rivington", "Bark, bark!!!", 20.5, 7);
+	Animal* ptrAnimal{ &newDog };
 
-	Animal myDog("Ricky", "Bark!", 30.5, 5);
-	myDog.displayReport();
-
-	myDog.playAnimal();
-	myDog.makeSound();
-	myDog.displayReport();
-
-	Dog myDog_2;
-	myDog_2.displayReport();
-
-	Dog myDog_3("Scratch");
-	myDog_3.displayReport();
-
+	ptrAnimal->makeSound();
+	ptrAnimal->displayReport();
 
 	return 0;
 }
